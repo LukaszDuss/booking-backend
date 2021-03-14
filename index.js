04cebb4ed4e9b829b8852393c10b5652acc474dd
LukaszDuss <lukasz.du2s@gmail.com>
2021-03-14 19:36:17 +0100

    up:dziala

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./queries");
+const port = 3002;
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3002
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
-
-app.get('/bookings', db.getBookings)
-app.get('/bookings/:id', db.getBookingById)
-app.post('/bookings', db.createBooking)
-app.put('/bookings/:id', db.updateBooking)
-app.delete('/bookings/:id', db.deleteBooking)
+app.get("/bookings", db.getBookings);
+app.get("/bookings/:id", db.getBookingById);
+app.post("/bookings", db.createBooking);
+app.put("/bookings", db.updateBooking);
+app.delete("/bookings", db.deleteBooking);
 
 app.listen(port, () => {
-  console.log(`App running on port http://localhost:${port}.`)
-})
+  console.log(`App running on port http://localhost:${port}.`);
+});
--- queries.js
+++ queries.js
@@ -7,77 +7,102 @@ const pool = new Pool({
   port: 5432,
 });
 
+const token = "3b243a78b06c0bf6083c6fdc6909c375";
 // GET ALL BOOKINGS
 
 function getBookings(request, response) {
-  pool.query(`SELECT * FROM bookings ORDER BY id ASC`, (error, result) => {
-    if (error) {
-      throw error;
-    }
-    console.log(result.rows);
-    response.status(200).json(result.rows);
-  });
+  if (request.headers.token != token) {
+    response.status(403).send("Unauthorised access!");
+  } else {
+    pool.query(`SELECT * FROM bookings ORDER BY id ASC`, (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(result.rows);
+    });
+  }
 }
 
 //GET BOOKING BY ID
 
 const getBookingById = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query(`SELECT * FROM bookings WHERE id = ${id}`, (error, result) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(result.rows);
-  });
+  if (request.headers.token != token) {
+    response.status(403).send("Unauthorised access!");
+  } else {
+    pool.query(
+      `SELECT * FROM bookings WHERE id = ${request.params.id}`,
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).json(result.rows);
+      }
+    );
+  }
 };
 
 //CREATE BOOKING
 
 const createBooking = (request, response) => {
-  const { name, email, fromDate, toDate, status } = request.body;
-
-  pool.query(
-    `INSERT INTO bookings (name, email, fromdate, todate, status) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
-    [name, email, fromDate, toDate, status],
-    (error, result) => {
-      if (error) {
-        throw error;
+  const { name, email, fromDate, toDate, status, price, product } = request.body;
+  if (request.headers.token != token) {
+    response.status(403).send("Unauthorised access!");
+  } else {
+    pool.query(
+      `INSERT INTO bookings (name, email, fromdate, todate, status, price, product) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`,
+      [name, email, fromDate, toDate, status, price, product],
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        response
+          .status(201)
+          .send(`Booking added with ID: ${result.rows[0].id}`);
       }
-      response.status(201).send(`Booking added with ID: ${result.rows[0].id}`);
-    }
-  );
+    );
+  }
 };
 
 //UPDATE BOOKING
 
 const updateBooking = (request, response) => {
-  const id = parseInt(request.params.id);
-  const status = parseInt(request.params.status);
-
-  pool.query(
-    `UPDATE bookings SET status = $2 WHERE id = $1`,
-    [id, status],
-    (error, result) => {
-      if (error) {
-        throw error;
+  const { id, status } = request.body;
+  if (request.headers.token != token) {
+    response.status(403).send("Unauthorised access!");
+  } else {
+    pool.query(
+      `UPDATE bookings SET status = $2 WHERE id = $1 `,
+      [id, status],
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        response
+          .status(200)
+          .send(`Booking status modified with ID: ${id}`);
       }
-      response.status(200).send(`Booking status modified with ID: ${id}`);
-    }
-  );
+    );
+  }
 };
 
 //DELETE BOOKING
 
 const deleteBooking = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query(`DELETE FROM bookings WHERE id = $1`, [id], (error, result) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).send(`Booking deleted with ID: ${id}`);
-  });
+  if (request.headers.token != token) {
+    response.status(403).send("Unauthorised access!");
+  } else {
+    pool.query(
+      `DELETE FROM bookings WHERE id = ${request.body.id} RETURNING id`,
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        response
+          .status(200)
+          .send(`Booking deleted with ID: ${request.body.id}`);
+      }
+    );
+  }
 };
 
 module.exports = {
